d384e2affb2b106e074f7305bc65435b9a414a64
Vetrivakai2025 <vetrivakai@gmail.com>
2025-09-23 11:57:51 +0530

    rm stp installs

--- routes/web.php
+++ routes/web.php
@@ -28,9 +28,7 @@ use App\Http\Controllers\ClientController;
 */
 
 
-$installed = Storage::disk('public')->exists('installed');
 
-if ($installed === true) {
 
     Auth::routes(['register' => false]);
 
@@ -380,76 +378,7 @@ if ($installed === true) {
 
     });
 
-        } else {
-
-            Route::get('/{vue?}',
-            function () {
-                    return redirect('/setup');
-            })->where('vue', '^(?!setup).*$');
-    
-            
-            Route::get('/setup', [
-                'uses' => 'SetupController@viewCheck',
-            ])->name('setup');
-
-            Route::get('/setup/step-1', [
-                'uses' => 'SetupController@viewStep1',
-            ]);
-
-            Route::post('/setup/step-2', [
-                'as' => 'setupStep1', 'uses' => 'SetupController@setupStep1',
-            ]);
-
-            Route::post('/setup/testDB', [
-                'as' => 'testDB', 'uses' => 'TestDbController@testDB',
-            ]);
-
-            Route::get('/setup/step-2', [
-                'uses' => 'SetupController@viewStep2',
-            ]);
-
-            Route::get('/setup/step-3', [
-                'uses' => 'SetupController@viewStep3',
-            ]);
-
-            Route::get('/setup/finish', function () {
-
-                return view('setup.finishedSetup');
-            });
-
-            Route::get('/setup/getNewAppKey', [
-                'as' => 'getNewAppKey', 'uses' => 'SetupController@getNewAppKey',
-            ]);
-
-            Route::get('/setup/getPassport', [
-                'as' => 'getPassport', 'uses' => 'SetupController@getPassport',
-            ]);
-
-            Route::get('/setup/getMegrate', [
-                'as' => 'getMegrate', 'uses' => 'SetupController@getMegrate',
-            ]);
-
-            Route::post('/setup/step-3', [
-                'as' => 'setupStep2', 'uses' => 'SetupController@setupStep2',
-            ]);
-
-            Route::post('/setup/step-4', [
-                'as' => 'setupStep3', 'uses' => 'SetupController@setupStep3',
-            ]);
-
-            Route::post('/setup/step-5', [
-                'as' => 'setupStep4', 'uses' => 'SetupController@setupStep4',
-            ]);
-
-            Route::post('/setup/lastStep', [
-                'as' => 'lastStep', 'uses' => 'SetupController@lastStep',
-            ]);
-
-            Route::get('setup/lastStep', function () {
-                return redirect('/setup', 301);
-            });
-
-    } 
+        
     
 
 
